ae190630605fe9580272902a9091f269f4d7a34c
ochelle <alexizochelle@gmail.com>
2024-01-11 14:19:42 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,10 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const errorModal = document.getElementById('modal');
+  const errorMessage = document.getElementById('modal-message');
+  const likeButtons = document.querySelectorAll('.like');
 
+  errorModal.classList.add('hidden'); // Hide the error modal initially
 
-
-
+  likeButtons.forEach(likeButton => {
+    likeButton.addEventListener('click', () => {
+      mimicServerCall()
+        .then(() => {
+          if (likeButton.classList.contains('activated-heart')) {
+            likeButton.classList.remove('activated-heart');
+            likeButton.textContent = EMPTY_HEART;
+          } else {
+            likeButton.classList.add('activated-heart');
+            likeButton.textContent = FULL_HEART;
+          }
+        })
+        .catch(error => {
+          errorMessage.textContent = error;
+          errorModal.classList.remove('hidden');
+          setTimeout(() => {
+            errorModal.classList.add('hidden');
+          }, 3000);
+        });
+    });
+  });
+});
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
